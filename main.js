a8903124a933a50747696e6943d58c439f31704c
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2021-08-24 00:44:41 +0100

    Update main.js
    end of episode 8

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 Vue.component('task-list', {
-  template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+  template: `
+
+    <div>
+
+      <task v-for="task in tasks">{{ task.task }}</task>
+
+    </div>
+    
+  `,
 
   data() {
     return {
