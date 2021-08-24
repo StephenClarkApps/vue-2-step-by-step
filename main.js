83f943c6c185f3f3a3e985ed1a967385417d983d
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2021-08-24 16:11:02 +0100

    Start of episode 10
    ...

--- main.js
+++ main.js
@@ -1,70 +1,21 @@
-Vue.component('message', {
-
-  props: ['title', 'body'],
-
-  data() {
-
-    return {
-
-          isVisable: true
-
-    };
-  },
+Vue.component('modal', {
 
   template: `
+    		<div class="modal is-active">
+    			<div class = "modal-background"></div>
+    			<div class = "modal-content">
+    				<div class="box">
+    					<p>
+    						Baby baby, why can't we just be together. Yeah e yeah e yeah.
+    					</p>
+    				</div>
+    			</div>
 
-    <article class="message" v-show="isVisable">
-      <div class="message-header">
-
-        {{ title }}
-
-        <button type="button" @click="isVisable = false">x</button>
-
-      </div>
-
-      <div class="message-body">
-
-        {{ body }}
-
-      </div>
-
-    </article>
-
+          <button class="modal-close"></button>
+    		</div>
   `
 
 });
-
-
-// Vue.component('task-list', {
-//   template: `
-//
-//     <div>
-//
-//       <task v-for="task in tasks">{{ task.task }}</task>
-//
-//     </div>
-//
-//   `,
-//
-//   data() {
-//     return {
-//       tasks: [
-//
-//         { task: 'Go to the store', complete: true },
-//         { task: 'Go to the email', complete: false },
-//         { task: 'Go to the farm', complete: true },
-//         { task: 'Go to work', complete: false },
-//
-//       ]
-//     };
-//   }
-//
-// });
-//
-//
-// Vue.component('task', {
-//   template: '<li><slot></slot></li>'
-// });
 
 new Vue({
     el: '#root'
