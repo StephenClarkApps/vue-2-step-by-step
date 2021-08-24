226e09e2625ee4ce85dc59d4c8ec01655556aa92
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2021-08-24 01:15:19 +0100

    ...
    ...

--- main.js
+++ main.js
@@ -1,33 +1,76 @@
-Vue.component('task-list', {
-  template: `
+Vue.component('message', {
 
-    <div>
-
-      <task v-for="task in tasks">{{ task.task }}</task>
-
-    </div>
-    
-  `,
+  props: ['title', 'body'],
 
   data() {
+
     return {
-      tasks: [
 
-        { task: 'Go to the store', complete: true },
-        { task: 'Go to the email', complete: false },
-        { task: 'Go to the farm', complete: true },
-        { task: 'Go to work', complete: false },
+          isVisable: true
 
-      ]
     };
+  },
+
+  template: `
+
+    <article class="message" v-show="isVisable">
+      <div class="message-header">
+
+        {{ title }}
+
+        <button type="button" @click="hideModal">x</button>
+
+      </div>
+
+      <div class="message-body">
+
+        {{ body }}
+
+      </div>
+
+    </article>
+
+  `,
+
+  methods: {
+    hideModal() {
+        this.isVisable =  false;
+    }
   }
 
 });
 
 
-Vue.component('task', {
-  template: '<li><slot></slot></li>'
-});
+// Vue.component('task-list', {
+//   template: `
+//
+//     <div>
+//
+//       <task v-for="task in tasks">{{ task.task }}</task>
+//
+//     </div>
+//
+//   `,
+//
+//   data() {
+//     return {
+//       tasks: [
+//
+//         { task: 'Go to the store', complete: true },
+//         { task: 'Go to the email', complete: false },
+//         { task: 'Go to the farm', complete: true },
+//         { task: 'Go to work', complete: false },
+//
+//       ]
+//     };
+//   }
+//
+// });
+//
+//
+// Vue.component('task', {
+//   template: '<li><slot></slot></li>'
+// });
 
 new Vue({
     el: '#root'
